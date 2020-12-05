a5e1b8f347d824b3444ba4b5e305f2c300b71739
lakshminarendran <lakshmi.n@ltts.com>
2020-12-05 15:26:06 +0530

    files

---
+++ 3_Implementation/test/tset_calculator.c
@@ -0,0 +1,68 @@
+#include "CUnit/Basic.h"
+#include <CUnit/CUnit.h>
+
+/* Modify these two lines according to the project */
+#include <calculator.h>
+#define PROJECT_NAME   "Scientific Calculator"
+
+/* Prototypes for all the test functions */
+
+//void test_login(void);
+void test_phone(void);
+void test_temperature(void);
+void test_sanitize_check(void);
+
+/* Start of the application test */
+int main() {
+  
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+
+
+  /* Add your test functions in this format for testing*/
+  //CU_add_test(suite, "login", test_login);
+  CU_add_test(suite, "phone", test_phone);
+  CU_add_test(suite, "temperature", test_temperature);
+  CU_add_test(suite, "sanitize", test_sanitize_check);
+
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
+
+
+/* Write all the test functions */
+
+
+struct Customer c={.phone="1234567890"};
+struct Customer d={.phone="123456"};
+struct Customer e={.phone="0123456789"};
+
+void test_phone(void) {
+  CU_ASSERT(1 == phone_check(c));
+  CU_ASSERT(0 == phone_check(d));
+  CU_ASSERT(0 == phone_check(e));
+}
+
+void test_temperature(void) {
+  CU_ASSERT(1 == temp_check());
+  CU_ASSERT(1 == temp_check());
+}
+
+void test_sanitize_check(void) {
+  CU_ASSERT(1 == sanitize());
+  CU_ASSERT(1 == sanitize());
+}
