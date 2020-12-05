5e00e690cf68a93d644edfe2367da72f24e1735b
99002617 <71552686+99002617@users.noreply.github.com>
2020-12-05 16:30:25 +0530

    Operators Update

--- 3_Implementation/src/calculator.c
+++ 3_Implementation/src/calculator.c
@@ -19,3 +19,37 @@ int division(int a, int b)
 {
     return a/b;
 }
+
+int andoperator(int operand1, int operand2 )
+{  
+}
+
+int oroperator(int operand1, int operand2)
+{
+
+}
+
+int notoperator(int operand1, int operand2)
+{
+
+}
+
+int nandoperator(int operand1, int operand2)
+{
+
+}
+
+int noroperator(int operand1, int operand2)
+{
+
+}
+
+int xoroperator(int operand1, int operand2)
+{
+
+}
+
+int xnoroperator(int operand1, int operand2)
+{
+
+}
