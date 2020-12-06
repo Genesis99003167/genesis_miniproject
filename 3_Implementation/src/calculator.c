9085134b46dcb572ceecc9df97075e6209d3234d
99003165 <lakshmi.n@ltts.com>
2020-12-06 12:38:57 +0530

    update

--- 3_Implementation/src/calculator.c
+++ 3_Implementation/src/calculator.c
@@ -108,7 +108,7 @@ int NOToperator(int operand1)
 
 int NANDoperator(int operand1, int operand2)
 {
-     return ~(operand1&operand2);
+     return ~(operand1&&operand2);
 }
 
 int NORoperator(int operand1, int operand2)
